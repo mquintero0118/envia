a39edf545d62cb04948b6f6380e0350be557fdb5
mquintero0118 <m.alejandro.quintero@gmail.com>
2022-05-10 22:23:51 -0500

    First data table works

--- envia/src/components/ordenes.vue
+++ envia/src/components/ordenes.vue
@@ -1,14 +1,35 @@
 <template>
   <v-container>
-    <template>
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :items-per-page="5"
-        class="elevation-1"
-      ></v-data-table>
-      <h2>{{items}}</h2>
-    </template>
+    <v-row no-gutters>
+      <v-col md="auto">
+        <v-card class="pa-2" elevation="0"> Numero de orden: </v-card>
+      </v-col>
+      <v-col lg="2">
+        <v-card class="pa-2" elevation="0" v-if="selected"> {{selected[0].name}} </v-card>
+      </v-col>
+    </v-row>
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      :items="items"
+      :single-select="singleSelect"
+      item-key="id"
+      show-select
+      class="elevation-1"
+      hide-default-footer
+    >
+    </v-data-table>
+     <v-divider class="mt-4 mb-4"></v-divider>
+    <v-data-table
+    :headers="headers2"
+    class="elevation-1"
+      hide-default-foote
+    >
+    </v-data-table>
+   
+
+    <v-btn>logCOnsole</v-btn>
+    <h2 v-if="selected">{{selected[0].items}}</h2>
   </v-container>
 </template>
 
@@ -19,106 +40,55 @@ export default {
   name: "ordenesMain",
   data() {
     return {
-        items: null,
-      headers: [
+      singleSelect: true,
+      selected: null,
+      itemsDetail: [],
+      items: [],
+      header2: [
         {
-          text: "Dessert (100g serving)",
+          text: "Sku",
+          align: "start",
+          sortable: false,
+          value: "sku",
+        },
+        {
+          text: "Nombre",
           align: "start",
           sortable: false,
           value: "name",
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
+        {
+          text: "Cantidad",
+          align: "start",
+          sortable: false,
+          value: "quantity",
+        },
+        {
+          text: "Precio",
+          align: "start",
+          sortable: false,
+          value: "price",
+        },
       ],
-      desserts: [
+      headers: [
         {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%",
+          text: "Numero de orden",
+          align: "start",
+          sortable: false,
+          value: "id",
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%",
+          text: "Nombre de la orden",
+          sortable: false,
+          value: "name",
+          align: "start",
         },
       ],
     };
   },
   mounted() {
-      const header1 = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyDbMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ"
+    const header1 =
+      "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyDbMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ";
     axios
       .get("https://eshop-deve.herokuapp.com/api/v2/orders", {
         headers: {
@@ -127,7 +97,10 @@ export default {
       })
       .then((res) => {
         console.log(res.data);
-        this.items = res.data.orders;
+        res.data.orders.forEach((element) => {
+          this.items.push(element);
+        });
+        console.log(this.items);
       })
       .catch((error) => {
         console.error(error);
