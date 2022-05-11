b3310c4d950bc7bea885442b9c118628d302a414
mquintero0118 <m.alejandro.quintero@gmail.com>
2022-05-10 23:47:20 -0500

    Added basic dialog structure

--- envia/src/components/ordenes.vue
+++ envia/src/components/ordenes.vue
@@ -5,7 +5,9 @@
         <v-card class="pa-2" elevation="0"> Numero de orden: </v-card>
       </v-col>
       <v-col lg="2">
-        <v-card class="pa-2" elevation="0" v-if="selected"> {{selected[0].name}} </v-card>
+        <v-card class="pa-2" elevation="0" v-if="selected">
+          {{ selected[0].name }}
+        </v-card>
       </v-col>
     </v-row>
     <v-data-table
@@ -19,17 +21,57 @@
       hide-default-footer
     >
     </v-data-table>
-     <v-divider class="mt-4 mb-4"></v-divider>
+    <v-divider class="mt-4 mb-4"></v-divider>
     <v-data-table
-    :headers="headers2"
-    class="elevation-1"
-      hide-default-foote
+      class="elevation-1"
+      :headers="header2"
+      hide-default-footer
+      :items="itemsDetail"
+      v-if="itemsDetail"
     >
+      <template v-slot:footer>
+        <v-divider class="mb-2"></v-divider>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="mx-2 mb-2"
+              fab
+              dark
+              color="indigo"
+              @click="dialog = !dialog"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon dark> mdi-plus </v-icon>
+            </v-btn>
+          </template>
+          <span>Agregar producto</span>
+        </v-tooltip>
+      </template>
     </v-data-table>
-   
+    <v-dialog v-model="dialog" width="35%">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Agregar producto
+        </v-card-title>
+        <v-card-text class="mt-5">
+          <v-row>
+            <v-col cols="6"> <v-text-field outlined dense label="Sku"></v-text-field></v-col>
+            <v-col cols="6"><v-text-field outlined dense label="Nombre"></v-text-field></v-col>
+          </v-row>
+         <v-row>
+            <v-col cols="6"> <v-text-field outlined dense label="Cantidad"></v-text-field></v-col>
+            <v-col cols="6"><v-text-field outlined dense label="Precio"></v-text-field></v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="addItem"> Guardar </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-    <v-btn>logCOnsole</v-btn>
-    <h2 v-if="selected">{{selected[0].items}}</h2>
+    <!-- <v-btn>logCOnsole</v-btn> -->
   </v-container>
 </template>
 
@@ -40,6 +82,7 @@ export default {
   name: "ordenesMain",
   data() {
     return {
+      dialog: false,
       singleSelect: true,
       selected: null,
       itemsDetail: [],
@@ -86,25 +129,43 @@ export default {
       ],
     };
   },
-  mounted() {
-    const header1 =
-      "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyDbMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ";
-    axios
-      .get("https://eshop-deve.herokuapp.com/api/v2/orders", {
-        headers: {
-          Authorization: header1,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-        res.data.orders.forEach((element) => {
-          this.items.push(element);
-        });
-        console.log(this.items);
-      })
-      .catch((error) => {
-        console.error(error);
+  watch: {
+    selected() {
+      this.itemsDetail = [];
+      console.log(this.selected);
+      this.selected[0].items.forEach((element) => {
+        this.itemsDetail.push(element);
       });
+      console.log(this.itemsDetail);
+    },
+  },
+  mounted() {
+    this.getProducts();
+  },
+  methods: {
+    addItem() {
+      this.dialog = !this.dialog;
+    },
+    getProducts() {
+      const header1 =
+        "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyDbMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ";
+      axios
+        .get("https://eshop-deve.herokuapp.com/api/v2/orders", {
+          headers: {
+            Authorization: header1,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          res.data.orders.forEach((element) => {
+            this.items.push(element);
+          });
+          console.log(this.items);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
 };
 </script>
